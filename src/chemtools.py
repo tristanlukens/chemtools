0e339cb5c5c091613349374f6a60f16c9344f549
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2024-06-20 08:48:32 +0200

    extracted code into main function

--- src/chemtools.py
+++ src/chemtools.py
@@ -1,12 +1,19 @@
-import lib.pH
 import lib.molar_weight
+import lib.pH
 
 from helpers.parsing import main_parser
 
+# Credit to https://mike.depalatis.net/blog/simplifying-argparse.html for the
+# code in the main function
 
-if __name__ == "__main__":
+
+def main():
     args = main_parser.parse_args()
     if args.subcommand is None:
         main_parser.print_help()
     else:
         args.func(args)
+
+
+if __name__ == "__main__":
+    main()
